ade70707a79021379666b873f72784c86b26c749
Glen Nelson <glen.nelson.1@gmail.com>
2026-06-21 12:51:01 -0600

    Corrected eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,33 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { 
+  globalIgnores(["dist/**", "build/**"]),
+  {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js, pluginReact }, 
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    plugins: {
+      react: pluginReact,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
     rules: {
+      ...js.configs.recommended.rules,
+
       ...pluginReact.configs.recommended.rules,
-      ...pluginReact.configs['jsx-runtime'].rules,
+      ...pluginReact.configs["jsx-runtime"].rules,
     },
   },
-  pluginReact.configs.flat.recommended,
-  globalIgnores(["dist/**", "build/**"])
 ]);
